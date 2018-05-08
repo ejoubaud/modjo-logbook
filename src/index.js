27841497fefd1e826805e3efc6c257384b21689b
ejoubaud <emmanuel.joubaud@gmail.com>
2018-05-08 23:49:05 +0200

    Add redux
    Ready with combineReducers as we'll need it for react-redux-firebase anyway (see https://github.com/prescottprue/react-redux-firebase)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import 'muicss/dist/css/mui.css';
 
 import App from './App';
+import uiReducer from './ui-reducer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Setup redux store
+const rootReducer = combineReducers({ ui: uiReducer });
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
