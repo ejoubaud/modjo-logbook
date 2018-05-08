ede9026bf748678a77464368299fd223b563b882
ejoubaud <emmanuel.joubaud@gmail.com>
2018-05-08 22:19:20 +0200

    Remove noisy palette=[object] from Button
    Since mui's Button passed all its props as html attributes, we'd get this noisy attr that's only used for styling.

--- src/ColoredButton.js
+++ src/ColoredButton.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import Button from 'muicss/lib/react/button';
 import styled from 'styled-components';
 
 const getCol = (name) => (props) => props.palette[name]
+
+// Filters out style-only prop palette,
+// to avoid the noisy palette=[object] html attr in the DOM
+const FilteredButton = (props) => {
+  const { palette, ...passedProps } = props;
+  return <Button {...passedProps}>{props.children}</Button>;
+}
 
 const raisedBase = `
   color: ${getCol('contrastText')};
   background-color: ${getCol('main')};
 `
 
-const RaisedButton = styled(Button)`
+const RaisedButton = styled(FilteredButton)`
   ${raisedBase}
   color: ${getCol('contrastText')};
   background-color: ${getCol('main')};
