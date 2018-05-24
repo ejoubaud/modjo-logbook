31fd82c387f7fdde5c6537f1e191e888b99403a4
ejoubaud <emmanuel.joubaud@gmail.com>
2018-05-24 10:37:20 +0200

    Customize header/logo/favicon

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,14 @@ import ReactTooltip from 'react-tooltip';
 import ColorSelector from './ColorSelector';
 import Plan from './Plan';
 import BoulderForm from './BoulderForm';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import colors from '../colors';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <img src={logo} alt="logo" />
     </header>
     <Container>
       {Object.keys(colors).map(color => (
