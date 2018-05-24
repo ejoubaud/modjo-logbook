53dac5c40fbeb07b7e0c0496130c2a61081c06f5
ejoubaud <emmanuel.joubaud@gmail.com>
2018-05-24 10:42:27 +0200

    Add firebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
 import 'muicss/dist/css/mui.css';
 
 import App from './components/App';
 import uiReducer from './ui-reducer';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAW4FfrqI-zc3L1DSTqC79dX_VuvBC2UjE',
+  authDomain: 'modjo-logbook.firebaseapp.com',
+  databaseURL: 'https://modjo-logbook.firebaseio.com',
+  projectId: 'modjo-logbook',
+  storageBucket: 'modjo-logbook.appspot.com',
+  messagingSenderId: '1098715116304',
+});
 
 // Setup redux store
 const rootReducer = combineReducers({ ui: uiReducer });
