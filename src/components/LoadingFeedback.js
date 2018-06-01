f2e5b8d7358f84202674715db350b6c1e60be9f5
ejoubaud <emmanuel.joubaud@gmail.com>
2018-06-01 11:58:33 +0200

    Add global progress bar on auth loading

--- src/components/LoadingFeedback.js
+++ src/components/LoadingFeedback.js
@@ -6,7 +6,9 @@ const LoadingFeedback = ({ isLoading }) => (
   isLoading && <LinearProgress style={{ position: 'fixed', top: 0, left: 0, right: 0 }} />
 );
 
-const mapStateToProps = ({ ui: { isLoading } }) => ({ isLoading });
+const mapStateToProps = ({ ui, firebase: { auth } }) => ({
+  isLoading: ui.isLoading || !auth.isLoaded,
+});
 
 const ConnectedLoadingFeedback = connect(mapStateToProps, {})(LoadingFeedback);
 
