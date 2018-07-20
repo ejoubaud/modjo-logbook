ca6134f664f3120d26260f97e3374ee755647170
ejoubaud <emmanuel.joubaud@gmail.com>
2018-07-20 10:56:35 +0200

    Use JSS instead of CSS for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
 import SignInButton from './SignInButton';
 import ColorSelector from './ColorSelector';
@@ -9,16 +10,15 @@ import ErrorFeedback from './ErrorFeedback';
 import LoadingFeedback from './LoadingFeedback';
 import logo from '../images/logo.png';
 import colors from '../models/colors';
-import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
+const App = ({ classes }) => (
+  <div className={classes.app}>
+    <header className={classes.appHeader}>
       <img src={logo} alt="logo" />
       <SignInButton />
     </header>
-    <div className="container">
-      <div className="button-bar">
+    <div className={classes.container}>
+      <div className={classes.buttonBar}>
         {Object.keys(colors).map(color => (
           <ColorSelector color={color} key={`button-${color}`} />
         ))}
@@ -29,10 +29,62 @@ const App = () => (
     </div>
     <ErrorFeedback />
     <LoadingFeedback />
-    <footer className="App-header">
       Modjo Logbook &ndash; Non-officiel
+    <footer className={classes.appHeader}>
     </footer>
   </div>
 );
 
-export default App;
+const styles = {
+  app: {
+    textAlign: 'center',
+  },
+
+  appHeader: {
+    backgroundColor: '#222',
+    padding: '20px',
+    color: 'white',
+  },
+
+  // .container stolen from muicss
+  container: {
+    boxSizing: 'border-box',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    paddingLeft: '15px',
+    paddingRight: '15px',
+    '&:after': {
+      clear: 'both',
+    },
+    '&:before, &:after': {
+      content: ' ',
+      display: 'table',
+    },
+    '@media (max-width: 543px)': {
+      padding: 0,
+    },
+    '@media (min-width: 544px)': {
+      maxWidth: '570px',
+    },
+    '@media (min-width: 768px)': {
+      maxWidth: '740px',
+    },
+    '@media (min-width: 992px)': {
+      maxWidth: '960px',
+    },
+    '@media (min-width: 1200px)': {
+      maxWidth: '1170px',
+    },
+  },
+
+  buttonBar: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignText: 'center',
+  },
+};
+
+const StyledApp = withStyles(styles)(App);
+
+export default StyledApp;
